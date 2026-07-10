5d43c922d21214f1eee670883e99dfab1f35580a
adams <adamskiptalam0@gmail.com>
2026-07-10 13:40:06 +0300

    login user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -6,12 +10,18 @@ import * as crypto from 'crypto';
 import type { RegisterDto } from './dto/register.dto.js';
 import type { UsersService } from 'src/users/users.service.js';
 import type { EmailService } from './email.service.js';
+import type { LoginDto } from './dto/login.dto.js';
+import type { User } from 'src/generated/prisma/client.js';
+import type { Response } from 'express';
+import type { StringValue } from 'ms';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private emailService: EmailService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
   ) { }
 
   async register(dto: RegisterDto) {
@@ -36,12 +46,78 @@ export class AuthService {
 
     void this.emailService.sendVerificationEmail(dto.email, verificationToken);
 
-    const { passwordHash: password, ...results } = user;
+    const { passwordHash: _passwordHash, ...results } = user;
 
     return {
       message:
         'Registration was successful. Please check your email to verify your account',
       data: results,
     };
+  }
+
+  async login(dto: LoginDto, res: Response) {
+    const user = await this.usersService.findByEmail(dto.email);
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const passwordMatch = await bcrypt.compare(dto.password, user.passwordHash);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    if (!user.isVerified) {
+      throw new UnauthorizedException(
+        'Please verify your email before logging in',
+      );
+    }
+
+    const tokens = await this.generateTokens(user);
+    await this.saveRefreshToken(user.id, tokens.refreshToken);
+    this.setRefreshTokenCookie(res, tokens.refreshToken);
+
+    return {
+      accessToken: tokens.accessToken,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+      },
+    };
+  }
+
+  private async generateTokens(user: User) {
+    const payload = { sub: user.id, email: user.email, role: user.role };
+    const accessToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.configService.get('JWT_ACCESS_EXPIRES_IN'),
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      expiresIn: this.configService.get('JWT_REFRESH_EXPIRES_IN'),
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  private async saveRefreshToken(id: string, refresh: string) {
+    const tokenHash = await bcrypt.hash(refresh, 12);
+    await this.usersService.update(id, {
+      verificationToken: tokenHash,
+    });
+  }
+
+  private setRefreshTokenCookie(res: Response, refreshToken: string) {
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
   }
 }
