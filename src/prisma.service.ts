eff6fea9ee64f15fb0aa334e988c1ec7c0920dd8
adams <adamskiptalam0@gmail.com>
2026-07-11 20:56:39 +0300

    configuration fixes. nothing works yet

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from './generated/prisma/client.js';
 import { PrismaNeon } from '@prisma/adapter-neon';
 // import { PrismaPg } from '@prisma/adapter-pg';
-// import { Pool } from '@neondatabase/serverless';
+// import {Pool} from 'pg';
+import {neonConfig} from '@neondatabase/serverless';
+import ws from 'ws';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaNeon({
-      connectionString: process.env.DATABASE_URL,
-    });
+    // neonConfig.webSocketConstructor = ws;
+    neonConfig.fetchConnectionCache = true;
+
+    const connectionString = process.env.DATABASE_URL;
+if (!connectionString) throw new Error('Missing DATABASE_URL');
+   const adapter = new PrismaNeon({connectionString});
+
     super({
       adapter,
       log: ['error', 'warn', 'query'],
